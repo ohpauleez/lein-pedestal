363411eed7174e99a8565b100a90f059f7aa01ab
Paul deGrandis <paul.degrandis@gmail.com>
2013-05-20 10:39:15 -0700

    Adding the web.xml attributes in

--- src/leiningen/pedestal/war.clj
+++ src/leiningen/pedestal/war.clj
@@ -44,7 +44,11 @@
     (let [server-ns (app-server-ns project)]
       (xml/indent-str
         (xml/sexp-as-element
-          [:web-app
+          [:web-app {"xmlns"              "http://java.sun.com/xml/ns/javaee"
+                     "xmlns:xsi"          "http://www.w3.org/2001/XMLSchema-instance"
+                     "xsi:schemaLocation" "http://java.sun.com/xml/ns/javaee http://java.sun.com/xml/ns/javaee/web-app_3_0.xsd"
+                     "version"            "3.0"
+                     "metadata-complete"  "true"}
            [:description (description project)]
            [:display-name (display-name project)]
            [:servlet
